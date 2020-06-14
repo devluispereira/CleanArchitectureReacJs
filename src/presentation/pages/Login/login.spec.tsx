e5ce1d3328b2e2fa224cd4110400ca56489e21c8
Luis Gustavo <devluispereira@gmail.com>
2020-06-14 17:40:51 -0300

    refactor: refatorando teste criando SUT

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes={
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login/>)
+  return {
+    sut
+  }
+}
 
 describe('Login Component', () => {
   test('should start with initial state', () => {
-    const { getByTestId } = render(<Login/>)
-    const spinner = getByTestId('errorWrap')
+    const { sut } = makeSut()
+    const spinner = sut.getByTestId('errorWrap')
     expect(spinner.childElementCount).toBe(0)
 
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
 
-    const emailStatusInput = getByTestId('email-status')
+    const emailStatusInput = sut.getByTestId('email-status')
     expect(emailStatusInput.title).toBe('Campo Obrigatório')
     expect(emailStatusInput.textContent).toBe('ok')
 
-    const passwordStatusInput = getByTestId('password-status')
+    const passwordStatusInput = sut.getByTestId('password-status')
     expect(passwordStatusInput.title).toBe('Campo Obrigatório')
     expect(passwordStatusInput.textContent).toBe('ok')
   });
